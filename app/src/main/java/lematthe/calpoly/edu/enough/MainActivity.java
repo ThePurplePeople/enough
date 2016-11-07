f2337e6dfcb5f0d8d9a7248d040701cb5d9a3ea9
lematthe28 <lematthe@calpoly.edu>
2016-11-07 08:07:55 -0800

    permissions done

--- app/src/main/java/lematthe/calpoly/edu/enough/MainActivity.java
+++ app/src/main/java/lematthe/calpoly/edu/enough/MainActivity.java
@@ -1,6 +1,7 @@
 package lematthe.calpoly.edu.enough;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -8,6 +9,7 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -15,22 +17,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     //declare globally, this can be any int
-    public final int MY_LOCATION_REQUEST = 456;
-    public final int MY_SMS_REQUEST = 123;
     public final int PICK_CONTACT = 2015;
+    public final int PERMISSION_ALL = 1;
+    public final int LOCATION_PERM = 2;
     String contact1;
+    String TAG = "here";
     Button sendButton;
     Button locationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //implicit layout inflator
         setContentView(R.layout.activity_main);
 
+        // carry on the normal flow, as the case of  permissions  granted.
         Button myContact1 = (Button) findViewById(R.id.contact1);
         sendButton = (Button) findViewById(R.id.send);
 
@@ -43,6 +52,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(i, PICK_CONTACT);
             }
         });
+
+        if(checkAndRequestPermissions()) {
+
+            sendButton.setEnabled(false);
+            locationButton.setEnabled(false);
+
+        }
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -57,38 +73,32 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-            Log.d("PLAYGROUND", "Permission is not granted, requesting");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, MY_SMS_REQUEST);
-            sendButton.setEnabled(false);
-        } else {
-            Log.d("PLAYGROUND", "Permission is granted");
-        }
-
         locationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getLocation();
+                Log.d("clicked", "get location");
             }
         });
-
-
     }
 
-    public void getLocation() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            Log.d("PLAYGROUND", "Permission is not granted, requesting");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST);
-            locationButton.setEnabled(false);
-        } else {
-            Log.d("PLAYGROUND", "Permission is granted");
+    private  boolean checkAndRequestPermissions() {
+        int permissionSendMessage = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
+        int locationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (locationPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
-
+        if (permissionSendMessage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.SEND_SMS);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), PERMISSION_ALL);
+            return false;
+        }
+        return true;
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -103,32 +113,92 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        Log.d(TAG, "Permission callback called-------");
         switch (requestCode) {
-            case MY_LOCATION_REQUEST: {
-                // If request is cancelled, the result arrays are empty.
+            case LOCATION_PERM: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("PLAYGROUND", "Permission has been granted");
-                    locationButton.setEnabled(true);
-                } else {
-                    Log.d("PLAYGROUND", "Permission has been denied or request cancelled");
-                    locationButton.setEnabled(false);
+                    Log.d("perm granted", "yaya");
                 }
-                return;
-            }
-            case MY_SMS_REQUEST: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("PLAYGROUND", "Permission has been granted");
-                    sendButton.setEnabled(true);
-                } else {
-                    Log.d("PLAYGROUND", "Permission has been denied or request cancelled");
-                    sendButton.setEnabled(false);
+                else {
+                    Log.d("boo", "poop");
                 }
             }
+            case PERMISSION_ALL: {
+                Map<String, Integer> perms = new HashMap<>();
+                // Initialize the map with both permissions
+                perms.put(Manifest.permission.SEND_SMS, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                // Fill with actual results from user
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i++)
+                        perms.put(permissions[i], grantResults[i]);
+                    // Check for both permissions
+                    if (perms.get(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED
+                            && perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Log.d(TAG, "sms & location services permission granted");
+                        // process the normal flow
+                        //else any one or both the permissions are not granted
+                    } else {
+                        Log.d(TAG, "Some permissions are not granted ask again ");
+                        //permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
+//                        // shouldShowRequestPermissionRationale will return true
+                        //show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                            showDialogOK("SMS and Location Services Permission required for this app",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            switch (which) {
+                                                case DialogInterface.BUTTON_POSITIVE:
+                                                    checkAndRequestPermissions();
+                                                    break;
+                                                case DialogInterface.BUTTON_NEGATIVE:
+                                                    // proceed with logic by disabling the related features or quit the app.
+                                                    locationButton.setEnabled(false);
+                                                    sendButton.setEnabled(false);
+                                                    break;
+                                            }
+                                        }
+                                    });
+                        }
+                        //permission is denied (and never ask again is  checked)
+                        //shouldShowRequestPermissionRationale will return false
+                        else {
+                            Toast.makeText(this, "Go to settings and enable permissions", Toast.LENGTH_LONG)
+                                    .show();
+                            locationButton.setEnabled(false);
+                            sendButton.setEnabled(false);
+                            //proceed with logic by disabling the related features or quit the app.
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void showDialogOK(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", okListener)
+                .create()
+                .show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(checkAndRequestPermissions()) {
+
+            sendButton.setEnabled(true);
+            locationButton.setEnabled(true);
 
         }
     }
 
 }
+
